7e2bc0e977cde0a55d23248d52192a4f879cc791
Kim Jaeho <scorve5322@gamil.com>
2024-02-07 15:25:41 +0900

    Feat Springboot

--- App/backend/src/main/java/com/example/backend/BackendApplication.java
+++ App/backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,6 +1,6 @@
 package com.example.backend;
 
-//testcode 1
+//testcode 12
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
