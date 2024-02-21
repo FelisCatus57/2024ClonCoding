80bdeba78a4ea8d6d3a3c08a1a8e98ee33b36889
Jaeho Kim <153428925+scorve12@users.noreply.github.com>
2024-02-22 00:27:57 +0900

    fix routerlink

--- App/frontend/src/App.js
+++ App/frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Sidebar from'./component/SideBar'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //모든 컴포넌트는 대문자로 시작해야한다.
 function App() {
@@ -10,9 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Sidebar/>
-      <Switch>
+      <Routes>
         <Route path='/'></Route>
-      </Switch>
+      </Routes>
     </BrowserRouter>
     
   );
