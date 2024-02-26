460a02ae69e7606c3c02ec4d87d598d46695626f
Jaeho Kim <153428925+scorve12@users.noreply.github.com>
2024-02-26 14:55:07 +0900

    feat add folder assets

--- App/frontend/src/component/SideBar.js
+++ App/frontend/src/component/SideBar.js
@@ -1,16 +1,24 @@
 import '../CSS/default.css'
 
+import IconHome from '../assets/home.svg'
+import IconSearch from '../assets/search.svg'
+import IconExplore from '../assets/Adventures.png'
+import IconReels from '../assets/Cinema.png'
+import IconMassage from '../assets/Email Send.png'
+import IconAlarm from '../assets/Heart.png'
+import IconMenu from '../assets/Vector.svg'
+
+
 export default function Sidebar() {
     return <nav>
-        <ul class = "buttonBox">
-            <li >Home</li>
-            <li>Search</li>
-            <li>Explore</li>
-            <li>Reels</li>
-            <li>Massage</li>
-            <li>Alarm</li>
-            <li>User</li>
+        <ul className = "buttonBox">
+            <li><img src={IconHome}/>Home</li>
+            <li><img src={IconSearch}/>Search</li>
+            <li><img src={IconExplore}/>Explore</li>
+            <li><img src={IconReels}/>Reels</li>
+            <li><img src={IconMassage}/>Massage</li>
+            <li><img src={IconAlarm}/>Alarm</li>
+            <li><img src={IconHome}/>User</li>
         </ul>
-        
     </nav>
 };
