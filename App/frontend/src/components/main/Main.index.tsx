54af22d7c2280419fae46b89297e716fe26933cd
computerkingminsu <ya5656@naver.com>
2024-03-16 16:40:25 +0900

    feat storycarousel, sidebar ui

--- App/frontend/src/components/main/Main.index.tsx
+++ App/frontend/src/components/main/Main.index.tsx
@@ -16,17 +16,11 @@ export default function Main(): JSX.Element {
       <Wrapper>
         <Story />
         <Contents />
-<<<<<<< HEAD
         {/* 스크롤 테스트..*/}
         <Contents />
         <Contents />
+        <Contents />
         {/* 스크롤 테스트..*/}
-=======
-        {/* 스크롤 테스트 .*/}
-        <Contents />
-        <Contents />
-        {/* 스크롤 테스트 .*/}
->>>>>>> 25149b7b4c631594f73d58dc759792624e65e8ff
       </Wrapper>
     </>
   );
